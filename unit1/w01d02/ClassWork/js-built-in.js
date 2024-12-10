592b1a4ca823e930659b5ac87c249a3084c0ab2f
Amos Yeo <amosyeo.sh@gmail.com>
2024-12-10 20:31:19 +0800

    Adding classwork for built-in

---
+++ unit1/w01d02/ClassWork/js-built-in.js
@@ -0,0 +1,22 @@
+// build in object >> Math
+Math.random();
+console.log(Math.random());
+
+let num = 1.21;
+console.log(Math.floor(num));
+
+console.log("comments",Math.floor(num));
+console.log   ("PI", Math.PI);
+
+let randomNumber = Math.random();
+const birthday = new Date("2023-05-31");
+console.log("BIRTHDAY",birthday);
+
+let year = birthday.getFullYear();
+console.log("YEAR",year); // 2023
+
+let time = birthday.getTime();
+console.log("TIME",time);
+
+let now = Date.now();
+console.log("NOW",now);
